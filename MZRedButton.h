d0495be4380040f5ebaada6e37e40587a3366bc8
Chensh <mail2chensh@gmail.com>
2018-11-05 09:25:39 +0800

    change

---
+++ MZRedButton.h
@@ -0,0 +1,16 @@
+//
+//  MZRedButton.h
+//  Pods-MZFBManager_Example
+//
+//  Created by Chensh on 2018/11/5.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MZRedButton : UIButton
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ MZRedButton.m
@@ -0,0 +1,20 @@
+//
+//  MZRedButton.m
+//  Pods-MZFBManager_Example
+//
+//  Created by Chensh on 2018/11/5.
+//
+
+#import "MZRedButton.h"
+
+@implementation MZRedButton
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
